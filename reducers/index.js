b4674d37c48fb242b170f0d60378d274d1d5ad72
tecmz <2131311518@qq.com>
2019-03-17 19:49:30 +0800

    init

---
+++ reducers/index.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var _redux=require("../npm/redux/lib/redux.js"),_store=require("./store.js"),_store2=_interopRequireDefault(_store);function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}exports.default=(0,_redux.combineReducers)({store:_store2.default});
